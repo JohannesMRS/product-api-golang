afe0c10ba729193b33decb636ce1daeac6aa6e5a
JohannesMRS <mariojr8304@gmail.com>
2026-09-11 14:41:06 +0700

    feat: adding product_service on service's folder

---
+++ services/product_service.go
@@ -0,0 +1,32 @@
+package services
+
+import (
+	"manajemen-product/models"
+	"manajemen-product/repositories"
+)
+
+type ProductService interface {
+	GetAllProducts() ([]models.Product, error)
+	CreateProduct(input models.CreateProductInput) (models.Product, error)
+}
+
+type productService struct {
+	repo repositories.ProductRepository
+}
+
+func NewProductService(repo repositories.ProductRepository) ProductService {
+	return &productService{repo: repo}
+}
+
+func (s *productService) GetAllProducts() ([]models.Product, error) {
+	return s.repo.GetAll()
+}
+
+func (s *productService) CreateProduct(input models.CreateProductInput) (models.Product, error) {
+	product := models.Product{
+		Name:  input.Name,
+		Price: input.Price,
+	}
+
+	return s.repo.Create(product)
+}
