f5f3360425f1742ed1b5ed738f1939896912ed7d
JohannesMRS <mariojr8304@gmail.com>
2026-09-13 19:55:43 +0700

    fix: resolve uncompatible json type on models

--- models/user.go
+++ models/user.go
@@ -8,8 +8,8 @@ import (
 type User struct {
 	ID       uuid.UUID `gorm:"type:char(36);primary_key" json:"id"`
 	Name     string    `gorm:"not null" json:"name"`
-	Email    string    `gorm:"unique; not null", json:"name"`
-	Password string    `gorm:"not null", json:"-"`
+	Email    string    `gorm:"unique; not null" json:"email"`
+	Password string    `gorm:"not null" json:"-"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
